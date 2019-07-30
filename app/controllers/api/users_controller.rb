2cd9d52fd545014033d30c5b1c04984c3a4e5806
hamzakhanane <khananehamza@gmail.com>
2019-07-30 11:57:18 -0400

    create Users table, controller, model, views and sessions controller, routes

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,20 @@
+class Api::UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(users_params)
+    if @user.save
+      login!(@user)
+      render "api/users/show"
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+
+  private
+  def users_params
+    params.require(:user).permit(:username, :password, :first_name, :last_name)
+  end
+end
