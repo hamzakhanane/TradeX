38c38273804c7e96dc941cfff470a5c1ec1fe241
hamzakhanane <khananehamza@gmail.com>
2019-08-01 13:22:54 -0400

    demo

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(username: "demoUser", first_name:"demo", last_name:"user", password:"hunter12", buying_power: 25435 )
