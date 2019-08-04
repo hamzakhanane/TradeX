42d7eb8dc61bb80c6e8125a2b3b2ec9472407d50
hamzakhanane <khananehamza@gmail.com>
2019-08-04 01:31:22 -0400

    add chart implementation, 1m, 1d, 1y

--- frontend/components/stock_info.jsx
+++ frontend/components/stock_info.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import {fetchInfo,fetchQoutes,fetchCharts} from "../util/stock_info_api_util"
+import Chart from "./chart";
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
 
 class StockInfo extends React.Component{
 
@@ -15,6 +17,8 @@ class StockInfo extends React.Component{
     };
     }
 
+    
+
     componentDidMount(){
         const stockId = this.props.match.params.stockId;
        
@@ -27,25 +31,63 @@ class StockInfo extends React.Component{
             fetchCharts(ticker, "1D")
                     .then(charts => {
                         const newData = Object.assign({}, this.state.charts);
-                        newData["1D"] = charts;
+                        newData["1D"] = this.structureData(charts);
                         this.setState({charts: newData})
                     });
             fetchCharts(ticker, "1M")
                 .then(charts => {
                     const newData = Object.assign({}, this.state.charts);
-                    newData["1M"] = charts;
+                    newData["1M"] = this.structureData(charts);
                     this.setState({ charts: newData })
                 });
             fetchCharts(ticker, "1Y")
                 .then(charts => {
                     const newData = Object.assign({}, this.state.charts);
-                    newData["1Y"] = charts;
+                    newData["1Y"] = this.structureData(charts);
                     this.setState({ charts: newData })
                 });
         })
 
 
     }
+
+    findMinClosing(data){
+       
+        let resultarr = data.map(data => data.close);
+        const resultarr2 = resultarr.filter(Number);
+        let result = Math.min.apply(Math, resultarr2);
+        
+        return result
+
+    }
+
+    findMaxClosing(data){
+        let resultarr = data.map(data => data.close);
+        let result = Math.max.apply(Math, resultarr);
+        
+        return result
+
+    }
+
+    structureData(data){
+        
+        let newdata = [];
+        for(let i=0; i<data.length; i++){
+            let obj = {
+               
+            };
+           
+            obj["date"]= data[i].date
+            obj["open"] = data[i].open
+            obj["close"] = data[i].close
+            newdata.push(obj);
+           
+        }
+        return newdata;
+       
+    }
+
+   
 
     // componentDidUpdate(prevPropsx, prevState) {
     //     const stockId = this.props.match.params.stockId;
@@ -85,15 +127,25 @@ class StockInfo extends React.Component{
         const day_char = this.state.charts["1D"];
         const month_char = this.state.charts["1M"];
         const year_char = this.state.charts["1Y"];
-
+        const min_closing = this.findMinClosing(year_char);
+        const max_closing = this.findMaxClosing(year_char);
+        
         debugger
         return(
+            
             <div>
                 <div>
                     <h2>{stockinfo.companyName}</h2>
                     <p>{qoute.latestPrice}</p>
                     <p>{qoute.change}{qoute.changePercent}</p>
                 </div>
+
+                <LineChart width={600} height={300} data={year_char}>
+                    <Line connectNulls={true} type="monotone" dataKey="close" dot={false} />
+                    <Tooltip/>
+                    <XAxis dataKey="date" />
+                    <YAxis domain={[min_closing, max_closing]} />
+                </LineChart>
 
 
 
